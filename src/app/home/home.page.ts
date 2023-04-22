b00678eab37372fd980c38b40d897b7858d74467
Larry Bluestoneapps <larry@bluestoneapps.com>
2023-04-22 16:09:30 -0400

    Added button to home screen and installed Capacitor plugin

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { NativeScreen } from 'my-native-screen-plugin';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
+  constructor() { }
 
-  constructor() {}
+  showHelloWorld() {
+    NativeScreen.showHelloWorld();
+  }
 
+  hideHelloWorld() {
+    NativeScreen.hideHelloWorld();
+  }
 }
